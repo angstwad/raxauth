db26ed5f09acbee9aa420b0d8b518be3497f231c
Paul Durivage <pauldurivage@gmail.com>
2012-12-04 21:54:44 -0600

    I forgot what I did

--- raxauth/__init__.py
+++ raxauth/__init__.py
@@ -14,7 +14,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = 'Paul Durivage <pauldurivage@gmail.com>'
+__author__ = 'Paul Durivage <paul.durivage@rackspace.com>'
 
 from .auth import auth
 from .exceptions import RAXAPIException
--- raxauth/exceptions.py
+++ raxauth/exceptions.py
@@ -1,4 +1,4 @@
-__author__ = 'Paul Durivage <pauldurivage@gmail.com>'
+__author__ = 'Paul Durivage <paul.durivage@rackspace.com>'
 
 class RAXAPIException(Exception):
     def __init__(self, errVal):
